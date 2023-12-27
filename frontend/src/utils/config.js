e87239a80db94158266e521ec1b132edc236495f
arsh459 <arshgoyal459@gmail.com>
2023-12-27 13:53:23 +0530

    frontend url fixes

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:4000/api/v1"
-// export const BASE_URL = "https://cute-plum-stingray-yoke.cyclic.app/dsapi/v1"
+// export const BASE_URL = "http://localhost:4000/api/v1"
+export const BASE_URL = "https://shiny-bear-vestments.cyclic.app/api/v1"
+// export const BASE_URL = "https://cute-plum-stingray-yoke.cyclic.app/api/v1"
